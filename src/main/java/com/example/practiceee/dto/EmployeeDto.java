ed8b57033f573634a84e949a21368bf26e0d474e
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-08 11:17:36 +0530

    create the EmployeeDto(using the lombok)

--- src/main/java/com/example/practiceee/dto/EmployeeDto.java
+++ src/main/java/com/example/practiceee/dto/EmployeeDto.java
@@ -1,4 +1,15 @@
 package com.example.practiceee.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDto {
+    private String name;
+    private String nic;
+    private int age;
+    private double salary;
 }
