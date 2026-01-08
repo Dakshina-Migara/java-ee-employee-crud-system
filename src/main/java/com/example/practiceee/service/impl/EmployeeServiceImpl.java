ce6405d6e19e428997e927ffe75852f7457500ca
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-08 11:33:10 +0530

    overide the methods from the interface

--- src/main/java/com/example/practiceee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/practiceee/service/impl/EmployeeServiceImpl.java
@@ -1,4 +1,33 @@
 package com.example.practiceee.service.impl;
 
-public class EmployeeServiceImpl {
+import com.example.practiceee.dto.EmployeeDto;
+import com.example.practiceee.service.EmployeeService;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService {
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+        return null;
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(EmployeeDto employeeDto) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteEmployee(String nic) {
+        return false;
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        return List.of();
+    }
+
+    @Override
+    public List<EmployeeDto> searchEmployees(String nic) {
+        return List.of();
+    }
 }
