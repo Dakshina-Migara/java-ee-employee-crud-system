b22c7d80dc4800543310f0b6fee242735484a974
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-08 12:37:07 +0530

    setup the saveEmployee impl

--- src/main/java/com/example/practiceee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/practiceee/service/impl/EmployeeServiceImpl.java
@@ -1,14 +1,38 @@
 package com.example.practiceee.service.impl;
 
+import com.example.practiceee.db.DBConnection;
 import com.example.practiceee.dto.EmployeeDto;
 import com.example.practiceee.service.EmployeeService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
-        return null;
+        String sql = "INSERT INTO employee(employee_nic, employee_name, employee_age, employee_salary) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DBConnection.getInstance().getConnection();
+             PreparedStatement pst = conn.prepareStatement(sql)) {
+
+            pst.setString(1, employeeDto.getNic());
+            pst.setString(2, employeeDto.getName());
+            pst.setInt(3, employeeDto.getAge());
+            pst.setDouble(4, employeeDto.getSalary());
+
+            int affectedRows = pst.executeUpdate();
+
+            if (affectedRows > 0) {
+                return employeeDto;
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
