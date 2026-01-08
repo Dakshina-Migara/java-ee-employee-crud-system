32a5dc538fdf2d1b72c0977a677cb5a0e5afa7cd
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-08 11:31:57 +0530

    create the methods get the data from user and send to the service

--- src/main/java/com/example/practiceee/servlet/EmployeeServlet.java
+++ src/main/java/com/example/practiceee/servlet/EmployeeServlet.java
@@ -1,4 +1,25 @@
 package com.example.practiceee.servlet;
 
-public class EmployeeServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/employee")
+public class EmployeeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
 }
